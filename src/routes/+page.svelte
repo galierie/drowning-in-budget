3ab7944692974dce33b4695a3e50dadefc487af1
Eriene Galinato <edgalinato@up.edu.ph>
2025-10-01 21:34:59 +0800

    feat: add project background

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -14,3 +14,10 @@
 <h2 class="text-2xl mb-6">Proposed Solutions</h2>
 <p>The group aims to analyze flood control projects from a data-driven perspective using the public dataset released by the government and correlate these to reports from the National Disaster Risk Reduction and Management Council (NDRRMC) to determine the potential tangible effect of these completed projects. These insights may be used not just to support calls for government accountability if the data shows that projects have been effective, but they may also provide next steps on what the government should invest into to truly improve disaster resilience.</p>
 <br />
+
+<h2 class="text-2xl mb-6">Background</h2>
+<p>The Philippines consistently ranks among the most disaster prone countries in the world. The 2025 World Risk Report listed the nation as the highest in disaster risk, particularly due to heavy exposure to river and coastal flooding across the entire archipelago (Bündnis Entwicklung Hilft, 2025).</p>
+<p>Disasters cannot necessarily be prevented, but their impact on people can be mitigated through infrastructure, planning, and other measures. The government has supposedly invested in flood control projects to improve river control, water drainage and storage, sediment control, and other forms of protection from flooding (Du, 2025). However, these projects have been mired with corruption, with the government admitting that many of these are “ghost” projects which have never actually been completed (Bacelonia, 2025).</p>
+<p>Actual experiences of citizens this rainy season also point to these projects’ ineffectiveness, leading to intensifying calls for accountability both online in social media and through major protests in the streets in recent months.</p>
+<p>As public pressure toward truly effective flood control mounts, it is essential to gain data-driven insights on how completed projects affect statistics such as casualties, injuries, and amount of damage to infrastructure.</p>
+<br />
