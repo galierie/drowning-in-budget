f7ef933f6277c53721ba9676245e637561ba6931
Eriene Galinato <edgalinato@up.edu.ph>
2025-10-01 21:33:53 +0800

    feat: add initial project info

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,5 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<h1 class="text-3xl text-center mb-8">Drowning in Budget: How Flood Control Budget Correlates to Damage Caused by Flooding</h1>
+<p class="text-center">Submitted by: CS 132 25.1 WFV Group 1</p>
+<p><b>Topic</b>: Flood Control Projects</p>
+<p><b>Sustainable Development Goal</b>: (11) Sustainable Cities and Communities: Urban Planning, Infrastructure Resilience, Environmental Sustainability</p>
+<br />
